ae8305b9dd1898cb3b6752bd11700d7a273ee31e
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2023-07-27 23:01:47 -0300

    Subindo banners + botão

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const banners = document.querySelectorAll(".banner");
-    let currentIndex = 0;
-  
-    function showBanner(index) {
-      banners.forEach((banner, i) => {
-        if (i === index) {
-          banner.classList.add("active");
-        } else {
-          banner.classList.remove("active");
-        }
-      });
-    }
-  
-    function nextBanner() {
-      currentIndex = (currentIndex + 1) % banners.length;
-      showBanner(currentIndex);
-    }
-  
-    function prevBanner() {
-      currentIndex = (currentIndex - 1 + banners.length) % banners.length;
-      showBanner(currentIndex);
-    }
-  
-    // Mostra o primeiro banner ao carregar a página
+document.addEventListener("DOMContentLoaded", function () {
+  const banners = document.querySelectorAll(".banner");
+  let currentIndex = 0;
+
+  function showBanner(index) {
+    banners.forEach((banner, i) => {
+      if (i === index) {
+        banner.classList.add("active");
+      } else {
+        banner.classList.remove("active");
+      }
+    });
+  }
+
+  function nextBanner() {
+    currentIndex = (currentIndex + 1) % banners.length;
     showBanner(currentIndex);
-  
-    // Alterna o banner a cada 3 segundos
-    setInterval(nextBanner, 3000);
-  });
-  
+  }
+
+  function prevBanner() {
+    currentIndex = (currentIndex - 1 + banners.length) % banners.length;
+    showBanner(currentIndex);
+  }
+
+  // Mostra o primeiro banner ao carregar a página
+  showBanner(currentIndex);
+
+  // Alterna o banner a cada 3 segundos
+  setInterval(nextBanner, 3000);
+});
